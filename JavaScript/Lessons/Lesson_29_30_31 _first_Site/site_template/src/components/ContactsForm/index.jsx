f6d9b230b1a38ea09192baf0d033532210b81cf7
ADMIN WIN10@WIN-GNKUB6IDL9R <pustezora@gmail.com>
2022-12-07 12:44:55 +0100

    useForm theorie

--- JavaScript/Lessons/Lesson_29_30_31 _first_Site/site_template/src/components/ContactsForm/index.jsx
+++ JavaScript/Lessons/Lesson_29_30_31 _first_Site/site_template/src/components/ContactsForm/index.jsx
@@ -1,27 +1,69 @@
 import React from 'react'
 import s from './index.module.css'
+import { useForm } from 'react-hook-form'
 
 export default function ContactsForm() {
+  
+  const { register, handleSubmit, formState: { errors } } = useForm({ mode: 'onBlur' });
+
+  const submit = (data) => console.log(data);
+  
+
+    const firstNameRegister = register('firstname', {
+        required: '* Field "firstname" is required'    
+    });
+    const lastNameRegister = register('lastname', {
+        required: '* Field "lastname" is required'
+    });
+
+    const messageRegister = register('message', {
+      required: '* Field "message" is required'
+  });
+  
+  
   return (
-    <form className={s.contacts_form}>
-      <div className={s.firstname_and_lastname}>
-        <label>
-          <p>Имя</p>
-          <input type='text' name='firstname' placeholder='Иван' />
-        </label>
+    <div>
+      <form className={s.contacts_form} onSubmit={handleSubmit(submit)}>
+        <div className={s.firstname_and_lastname}>
+          <label>
+            <p>Имя</p>
+            <input 
+            type='text' 
+            name='firstname' 
+            placeholder='Иван'  
+            {...firstNameRegister}
+          />
+          </label>
+
+          <label>
+            <p>Фамилия</p>
+            <input 
+            type='text' 
+            name='lastname' 
+            placeholder='Иванов' 
+            {...lastNameRegister} 
+          />
+          </label>
+        </div>
 
         <label>
-          <p>Фамилия</p>
-          <input type='text' name='lastname' placeholder='Иванов' />
+          <p>Сообщение</p>
+          <textarea 
+          type='text' 
+          name='message' 
+          placeholder='Ваше сообщение' 
+          {...messageRegister}
+        />
         </label>
+
+        <button>Отправить сообщение</button>
+      </form>
+
+      <div className={s.error_message}>
+        {errors.firstNameRegister && <p>{errors.firstNameRegister?.message}</p>}
+        {errors.lastNameRegister && <p>{errors.lastNameRegister?.message}</p>}
+        {errors.messageRegister && <p>{errors.messageRegister?.message}</p>}
       </div>
-
-      <label>
-        <p>Сообщение</p>
-        <textarea type='text' name='message' placeholder='Ваше сообщение' />
-      </label>
-
-      <button>Отправить сообщение</button>
-    </form>
+    </div>
   )
 }
